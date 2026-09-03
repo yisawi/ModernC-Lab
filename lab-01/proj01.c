15aaf3e564bb133add4a421302ee80072dddbcfc
Yusuf <contact@yisawi.me>
2026-09-04 00:49:16 +0300

    print a checkmark

---
+++ lab-01/proj01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    /**********************************************************
+    * Name: proj01 *
+    * Purpose: Prints a Checkmark. *
+    * Author: Me *
+    **********************************************************/
+    printf("\t   *\n");
+    printf("\t  *\n");
+    printf("\t *\n");
+    printf("*\t*\n");
+    printf("  *    *  \n");
+    printf("     *  \n");
+}
