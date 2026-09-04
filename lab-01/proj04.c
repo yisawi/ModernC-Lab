e37885c07c13d6e0bc9779360677399cd05e7702
Yusuf <contact@yisawi.me>
2026-09-04 03:59:23 +0300

    feat: update the polynomial equation using Horner's Rule.

--- lab-01/proj04.c
+++ lab-01/proj04.c
@@ -10,7 +10,7 @@ int main(void) {
     int x, eq;
     printf("Enter the value of x: ");
     scanf("%d", &x);
-    eq = (3 * x * x * x * x * x) + (2 * x * x * x * x) + (5 * x * x * x) + (x * x) + (7 * x) - 6;
+    eq = (((((3 * x + 2) * x - 5) * x - 1) * x + 7) * x - 6);
     printf("the result is: %d\n", eq);
     
     return 0;
