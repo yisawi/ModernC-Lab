e1aedfa69d68c20453e2a4834a1312dcadbebd5f
Yusuf <contact@yisawi.me>
2026-09-06 22:49:59 +0300

    implement digit reversal using arithmetic operators

---
+++ lab-03/proj01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int num;
+    printf("Enter a two-digit number: ");
+    scanf("%d", &num);
+    
+    printf("The reversal is: %d%d\n", (num % 10), (num / 10));
+}
